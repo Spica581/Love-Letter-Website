8d7fe6350fd659a9b59437dd8fe94783f19f2a8e
Sebastian Nicholas B. Ballesteros <sebastianballesteros127@gmail.com>
2026-02-07 22:34:00 +0800

    Set up routing for Home, Login, Signup, and LetterView

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,20 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import LetterView from './pages/LetterView';
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/letter/:id" element={<LetterView />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
