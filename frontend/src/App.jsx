54cf6be2d0155daa5f3578cd2fe35b87696505ff
Sebastian Nicholas B. Ballesteros <sebastianballesteros127@gmail.com>
2026-02-10 20:08:35 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Signup from './pages/Signup';
 import LetterView from './pages/LetterView';
 
 function App() {
@@ -10,7 +9,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
         <Route path="/letter/:id" element={<LetterView />} />
       </Routes>
     </Router>
